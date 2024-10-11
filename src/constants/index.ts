df086339c9c6a0b0de4a82e3a431e0c592a27fe5
daovinhlinh <linh142000@gmail.com>
2024-10-11 15:06:47 +0700

    Add more CustomReason

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -24,5 +24,6 @@ export enum RollResultType {
 export const CustomReasonPhrases = {
   PASSWORD_NOT_MATCH: 'Password not match',
   WRONG_USERNAME_PASSWORD: 'Wrong username or password',
-  ALREADY_CHECKED_IN: 'User already checked in'
+  ALREADY_CHECKED_IN: 'User already checked in',
+  DAY_IS_EXISTED: 'Day is existed'
 }
